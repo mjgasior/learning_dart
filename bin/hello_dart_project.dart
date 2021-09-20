f43b7dbb5d3bbba2ea73aa0f9250ee7e3bbf6e40
Michal J. Gasior <michaljgasior@gmail.com>
2021-09-20 12:54:08 +0200

    Chapter 2 - variables

--- bin/hello_dart_project.dart
+++ bin/hello_dart_project.dart
@@ -1,5 +1,18 @@
+import 'dart:math';
+
 void main(List<String> arguments) {
-  print(22 / 7); // regular division that gives 3.14...
-  print(22 ~/ 7); // integral division that gives 3
+  double angle = 45.45;
+
+  print(sin(angle.round() * pi / 180));
+
+  int base = 22;
+
+  print(base / 7); // regular division that gives 3.14...
+  print(base ~/ 7); // integral division that gives 3
+
+  if (base.isEven) {
+    print('The base is even!');
+  }
+
   print('Hello world!');
 }
