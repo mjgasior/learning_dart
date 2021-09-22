79973532410a97c2eacc9ee53bc673fe49af263a
Michal J. Gasior <michaljgasior@gmail.com>
2021-09-22 09:41:29 +0200

    Chapter 5 - named parameters

--- bin/hello_dart_project.dart
+++ bin/hello_dart_project.dart
@@ -3,11 +3,15 @@ import 'dart:math';
 void main() {
   print(fullName('Johnny', 'Good', 'Doc'));
   print(fullName('Peter', 'Steele'));
+
+  print(withinTolerance(15));
+  print(withinTolerance(15, min: 0, max: 20));
 }
 
-String fullName(String first, String last, [String? title]) {
-  if (title != null) {
-    return '$title $first $last';
-  }
-  return '$first $last';
+String fullName(String first, String last, [String title = 'sir']) {
+  return '$title $first $last';
+}
+
+bool withinTolerance(int value, {int min = 0, int max = 10}) {
+  return min <= value && value <= max;
 }
